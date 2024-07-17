7142cfd0e21b7be186bb175d58782d036a2045f1
Stephanie Castro <fp.daw.ies.ciudad.escolar@gmail.com>
2024-07-18 00:38:59 +0200

    Primer commit con React

---
+++ src/services/api.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+const ApiComponent = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users") // Cambia '/endpoint' por el endpoint de tu API
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Datos de la API:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default ApiComponent;
